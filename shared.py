cc7e763ef1efb06acac6e9727276e990bb13e039
borgler <borgler@users.noreply.github.com>
2013-10-06 20:14:18 -0700

    Added renaming/resizing defaults

--- shared.py
+++ shared.py
@@ -7,4 +7,5 @@ format_dict = {'JPEG': {'optimize': True, 'quality': 95},
               'PNG':  {'optimize': True},
               }
 
-options = {'format': 'JPEG', 'archive': False, 'src_dir': '', 'dest_dir': ''}
+options = {'format': 'JPEG', 'archive': False, 'src_dir': '', 'dest_dir': '',
+           'extensions': True}
